9ab7bd043bb5322902d389ac6a41bcdc03d0592a
Marcelo Ferrari Gonçalves <marceloferraryg@gmail.com>
2026-04-26 14:27:26 -0300

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -7,13 +7,17 @@ const withPWA = withPWAInit({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: isDev, 
+  disable: isDev,
 })
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  allowedDevOrigins: ['192.168.0.127'],
-}
 
+  // 👇 necessário para acessar no celular (dev)
+  allowedDevOrigins: ['192.168.0.127'],
+
+  // 👇 resolve conflito Turbopack x Webpack na build (Vercel)
+  turbopack: {},
+}
 
 export default withPWA(nextConfig)
